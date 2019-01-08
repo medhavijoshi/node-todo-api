01df78cab125f3dc0f66d9d0026ffced5b4b2314
Medhavi Joshi <medhavi.joshi1@gmail.com>
2019-01-08 02:38:18 -0800

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,3 +5,5 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+//process.env.NODE_ENV === 'development' || 'test'
